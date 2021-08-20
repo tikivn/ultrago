ccca4100a378d4892d84ff50fb2f505d54ef9c2c
n2tismee <thien.nguyen5@tiki.vn>
2021-08-20 08:19:27 +0700

    Clean package

--- email/validate.go
+++
@@ -1,8 +0,0 @@
-package email
-
-import "regexp"
-
-func IsEmailValid(e string) bool {
-	emailRegex := regexp.MustCompile(`[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+`)
-	return emailRegex.MatchString(e)
-}
